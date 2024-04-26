2c103dc2827e6d460a72e7661303112e35901ade
jm-programming <juanm.pineda@outlook.com>
2024-04-26 17:56:36 -0400

    LISTO

--- js/funciones.js
+++ js/funciones.js
@@ -2,29 +2,32 @@ const screen = document.getElementById("screen")
 
 const insertar = (num)  => {
     if(num == '.' && screen.value.includes('.')) return
-    if(screen.value == "" && (num == '*' || num == '/') ) return
+    if(screen.value == "" && (num == '*' || num == '/'|| num == '+') ) return
     if(screen.value.endsWith('-') && num =='-') return
     if(screen.value.endsWith('*') && num =='*') return
     if(screen.value.endsWith('/') && num =='/') return
-    if(screen.value.endsWith('+') && num =='+') return
+    if(screen.value.endsWith('+') && num == '+') return
+    
 
-    if(screen.value.substring(screen.value.length,-1) == '-' && num == '-' ) return
+    if ((screen.value.endsWith('+') ||
+        screen.value.endsWith('-') ||
+        screen.value.endsWith('*') ||
+        screen.value.endsWith('/')) && (
+        num == '-' ||
+        num == '+' ||
+        num == '*' ||
+        num == '/')) return
+    
     screen.value += num
-
 }
 
 
 const limpiar = () => screen.value = ''
 
-//si exp es un numero puede proceder al calculo, de lo contrario no hacer nada
 
 const igual = () => {
     const exp = screen.value;
-    const caracter = exp.substring(0, 1); 
-    if (caracter == "-" || !isNaN(caracter)) {
-        exp ? screen.value = eval(exp) : 0
-        
-    } 
+    exp ? screen.value = eval(exp) : 0  
 }
 
 const eliminar_digito = () => {
